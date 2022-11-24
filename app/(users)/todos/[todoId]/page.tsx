4fe4ff64f0eaa7e357db6619d556308e971f1c6c
emdile <emdilespring@gmail.com>
2022-11-24 16:34:54 +0000

    delete lines

--- app/(users)/todos/[todoId]/page.tsx
+++ app/(users)/todos/[todoId]/page.tsx
@@ -1,59 +1,40 @@
-import React from "react"
-import { Todo } from "../../../../typings"
-import { notFound } from 'next/navigation'
+import React from 'react';
+import { Todo } from '../../../../typings';
+import { notFound } from 'next/navigation';
 
-export const dynamicParams = true
+export const dynamicParams = true;
 
 type TodoPageProps = {
-    params: {
-        todoId: string
-    }
-}
+	params: {
+		todoId: string;
+	};
+};
 
 const fetchTodo = async (todoId: string) => {
+	const todo: Todo = await fetch(
+		`https://jsonplaceholder.typicode.com/todos/${todoId}`,
+		{ next: { revalidate: 60 } }
+	).then((res) => res.json());
 
-    // server side rendering prev
-    // const todo1: Todo = await
-    //     fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, {cache: 'no-cache'})
-    //         .then(res => res.json())
-
-    // static side rendering
-    // const todo2: Todo = await
-    //     fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, { cache: 'force-cache' })
-    //         .then(res => res.json())
-
-    // ISR rendering in sec
-    // const todo3: Todo = await
-    //     fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, { next: {revalidate: 60} })
-    //         .then(res => res.json())
-
-
-    // server side rendering
-    const todo: Todo = await
-        fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, { next: { revalidate: 60 } })
-            .then(res => res.json())
-
-    return todo
-}
+	return todo;
+};
 
 const TodoPage = async ({ params: { todoId } }: TodoPageProps) => {
-    const todo = await fetchTodo(todoId)
+	const todo = await fetchTodo(todoId);
 
-    if (!todo.id) return notFound()
+	if (!todo.id) return notFound();
 
-    return (
-        <div>Todo page: {todo.id}</div>
-    )
-}
+	return <div>Todo page: {todo.id}</div>;
+};
 
-export default TodoPage
+export default TodoPage;
 
 export const generateStaticParams = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos/')
-    const todos: Todo[] = await res.json()
-    const Todomin = todos.slice(0, 10)
+	const res = await fetch('https://jsonplaceholder.typicode.com/todos/');
+	const todos: Todo[] = await res.json();
+	const Todomin = todos.slice(0, 10);
 
-    return Todomin.map(t => ({
-        todoId: t.id.toString()
-    }))
-}
+	return Todomin.map((t) => ({
+		todoId: t.id.toString(),
+	}));
+};
